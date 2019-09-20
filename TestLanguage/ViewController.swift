7219942dc1af3eef8727996304aeec286098c103
Cuong <cuongdm@toprate.ios>
2019-09-20 21:22:43 +0700

    first commit

---
+++ TestLanguage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestLanguage
+//
+//  Created by ManhCuong on 9/20/19.
+//  Copyright © 2019 encodejsc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
